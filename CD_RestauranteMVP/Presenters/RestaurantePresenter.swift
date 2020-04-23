01661636ebde916c834680239881e9e565370bd0
Miguel Gallego <Miguel.0.1.1.2.3.5.8@gmail.com>
2020-04-23 07:16:54 +0200

    First Commit

---
+++ CD_RestauranteMVP/Presenters/RestaurantePresenter.swift
@@ -0,0 +1,9 @@
+//
+//  RestaurantePresenter.swift
+//  CD_RestauranteMVP
+//
+//  Created by Miguel on 23/04/2020.
+//  Copyright © 2020 Miguel Gallego Martín. All rights reserved.
+//
+
+import Foundation
---
+++ CD_RestauranteMVP/Views/RestauranteVC.swift
@@ -0,0 +1,30 @@
+//
+//  RestauranteVC.swift
+//  CD_RestauranteMVP
+//
+//  Created by Miguel on 23/04/2020.
+//  Copyright © 2020 Miguel Gallego Martín. All rights reserved.
+//
+
+import UIKit
+
+class RestauranteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
